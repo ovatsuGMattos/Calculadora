31109cf9faa50605bc8ea38a2d56e8a4ab132970
ovatusGMattos <gustavo.j.mattos1@gmail.com>
2025-03-11 12:53:03 -0300

    Add project files.

---
+++ Calculadora.ConsoleAPP/Program.cs
@@ -0,0 +1,96 @@
+﻿
+namespace Calculadora.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true) // enquanto verdadeiro for verdadeiro
+            {
+                Console.Clear();
+                Console.WriteLine("--------------------------------");
+                Console.WriteLine("Calculadora Tabajara 2025");
+                Console.WriteLine("--------------------------------");
+
+                Console.WriteLine("1 - Somar");
+                Console.WriteLine("2 - Subtrair");
+                Console.WriteLine("3 - Multiplicação");
+                Console.WriteLine("4 - Divisão");
+                Console.WriteLine("5 - Tabuada");
+                Console.WriteLine("S - Sair");
+
+                Console.WriteLine();
+                Console.Write("Escolha uma opção: ");
+
+                string opcao = Console.ReadLine().ToUpper();
+
+                if (opcao == "S")
+                    break;
+
+                else if (opcao == "5")
+                {
+                    Console.WriteLine("--------------------------------");
+                    Console.WriteLine("Tabuada");
+                    Console.WriteLine("--------------------------------");
+
+                    Console.Write("Digite o número: ");
+                    int numeroTabuada = Convert.ToInt32(Console.ReadLine());
+
+                    for (int contador = 1; contador <= 10; contador++)
+                    {
+                        int resultadoTabuada = numeroTabuada * contador;
+
+                        Console.WriteLine($"{numeroTabuada} x {contador} = {resultadoTabuada}");
+                    }
+
+                    Console.ReadLine();
+
+                    continue;
+                }
+
+                Console.Write("Digite o primeiro número: ");
+                string primeiroNumeroString = Console.ReadLine();
+                decimal primeiroNumero = Convert.ToDecimal(primeiroNumeroString);
+
+                Console.Write("Digite o segundo número: ");
+                string segundoNumeroString = Console.ReadLine();
+                decimal segundoNumero = Convert.ToDecimal(segundoNumeroString);
+
+                decimal resultado = 0;
+
+                if (opcao == "1")
+                    resultado = primeiroNumero + segundoNumero;
+
+                else if (opcao == "2")
+                    resultado = primeiroNumero - segundoNumero;
+
+                else if (opcao == "3")
+                    resultado = primeiroNumero * segundoNumero;
+
+                else if (opcao == "4")
+                {
+                    while (segundoNumero == 0)
+                    {
+                        Console.Write("Não é possível dividir por 0\n Digite o segundo número novamente -> ");
+
+                        segundoNumero = Convert.ToDecimal(Console.ReadLine());
+                    }
+
+                    resultado = primeiroNumero / segundoNumero;
+                }
+
+                Console.WriteLine("--------------------------------");
+                Console.WriteLine("Resultado: " + resultado.ToString("F2"));
+                Console.WriteLine("--------------------------------");
+
+                Console.Write("Deseja continuar? (S/N): ");
+                string opcaoContinuar = Console.ReadLine().ToUpper();
+
+                if (opcaoContinuar != "S")
+                    break;
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
